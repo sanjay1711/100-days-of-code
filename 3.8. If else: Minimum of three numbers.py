a195d18ff66612a5a218521786dc1340b2d33695
sanjay1711 <42834646+sanjay1711@users.noreply.github.com>
2019-08-06 20:57:21 +0530

    Create 3.8. If else: Minimum of three numbers.py

---
+++ 3.8. If else: Minimum of three numbers.py
@@ -0,0 +1,12 @@
+# Read an integer:
+a = int(input())
+b = int(input())
+c = int(input())
+# Print a value:
+# print(a)
+if(a<b and a<c):
+  print(a)
+elif(b<a and b<c):
+  print(b)
+else:
+  print(c)
