53cb6d78874d4a28ee1835ce7dd691d4109a209d
sanjay1711 <42834646+sanjay1711@users.noreply.github.com>
2019-08-06 20:48:02 +0530

    Create 1.5. Input print:Apple sharing.py

---
+++ 1.5. Input print:Apple sharing.py
@@ -0,0 +1,13 @@
+# Read the numbers like this:
+# n = int(input())
+
+# Print the result with print()
+
+# Example of division, integer division and remainder:
+# See output below for actual values 
+a = int(input())
+b = int(input())
+
+print(b // a)
+print(b % a)
+#x = int(input("enter the number of student")) #6
