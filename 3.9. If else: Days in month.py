0f99b9983604729c34d4dcbc2044b93719273bd1
sanjay1711 <42834646+sanjay1711@users.noreply.github.com>
2019-08-06 20:55:17 +0530

    Create 3.9. If else: Days in month.py

---
+++ 3.9. If else: Days in month.py
@@ -0,0 +1,9 @@
+
+month = int(input())
+
+if(month==2):
+  print(28)
+elif(month==1 or month==3 or month==5 or month==5 or month==7 or month==8 or month==10 or month==12):
+  print(31)
+else:
+  print(30)
