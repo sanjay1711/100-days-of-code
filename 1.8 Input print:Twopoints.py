e85faf7b736f0886145e976c529f9e3d7c20137a
sanjay1711 <42834646+sanjay1711@users.noreply.github.com>
2019-08-06 20:52:52 +0530

    Create 1.8 Input print:Twopoints.py

---
+++ 1.8 Input print:Twopoints.py
@@ -0,0 +1,13 @@
+import math
+# get the x coordinate of Point 1
+x1 = int(input())
+# get the y coordinate of Point 2
+y1 = int(input())  
+# get the x coordinate of Point 2
+x2 = int(input())  
+# get the y coordinate of Point 2
+y2 = int(input())  
+# Write the code to calculate distance between 
+# the two points 
+distance =( (x2-x1)**2+(y2-y1)**2)**0.5 
+print(distance)
